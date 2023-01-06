7da2e03ac79ae1ad964eeef891569a4d94ac997a
Onji-K <xodks672@gmail.com>
2023-01-06 14:40:40 +0900

    Initial commit

---
+++ src/test/java/net/onjee/basic/SpringbootSecurityFormOauthSessionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.onjee.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityFormOauthSessionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
